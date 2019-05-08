5780e269f6b9d228abbf5353bcbd5f1e863f26d6
Francesca Chater <francescachater@gmail.com>
2019-05-08 15:29:26 +0100

    Closing 8: added journey feature

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,32 @@
 
 class Oystercard
-  attr_reader :balance
+  MAXIMUM_AMOUNT = 90
+  attr_reader :balance, :journey
 
-  def initialize(balance= 0)
+  def initialize(balance= 0, journey = false)
     @balance = balance
+    @journey = journey
   end
 
   def top_up(n)
+    raise "top up (#{MAXIMUM_AMOUNT}) limit reached" if @balance + n > MAXIMUM_AMOUNT
     @balance += n
   end
+
+  def deduct(n)
+    @balance -= n
+  end
+
+  def in_journey?
+    @journey
+  end
+
+  def touch_in
+    @journey = true
+  end
+
+  def touch_out
+    @journey = false
+  end 
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,15 +1,51 @@
 require 'oystercard'
 
 describe Oystercard do
+subject(:card) {described_class.new}
 
-it "has a balance" do
-  expect(subject.balance).to eq(0)
-end
 
-describe '#top_up' do
-  it "allows customer to top up" do
-  expect(subject).to respond_to(:top_up).with(1).argument
+  it "has a balance" do
+    expect(subject.balance).to eq(0)
   end
-end
+
+  describe '#top_up' do
+    it "allows customer to top up" do
+      expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+    it "raise an error when limit reached" do
+      subject.top_up(90)
+      expect{ subject.top_up(1) }.to raise_error("top up (#{Oystercard::MAXIMUM_AMOUNT}) limit reached")
+    end
+  end
+
+  describe "#deduct" do
+    it "deducts from oyster card balance" do
+    card = Oystercard.new(50)
+    card.deduct(20)
+    expect(card.balance).to eq(30)
+    end
+  end
+
+  # describe "#in_journey?" do
+  #   it "checks journey status" do
+  #   expect(subject.in_journey?).to eq journey
+  #   end
+  # end
+
+  describe "#touch_in" do
+      it "start journey" do
+      #card = Oystercard.new
+      card.touch_in
+      expect(card).to be_in_journey
+      end
+    end
+
+    describe "#touch_out" do
+      it "ends journey" do
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+  end
 
 end
